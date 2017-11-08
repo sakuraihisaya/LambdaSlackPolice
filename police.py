f9515a1248d11f7356d4ce994e440bacee2226d6
Hisaya Sakurai <hisa.92.sakurai@gmail.com>
2017-11-08 16:35:40 +0900

    lambda版に更新

--- police.py
+++ police.py
@@ -31,17 +31,21 @@ class SlackPolice(object):
         """
         Delete all files in the slack team.
         """
+        channel_set = set()
         files = self.get_file_list()
         for file in files:
             if file['filetype'] == 'text':  # without code snippet
                 continue
             res = self.delete_file(file['id'])
+            channel_set.update(file['channels'])#重複しない
+
             if 'ok' in res and res['ok']:
                 print('{0} deleted.'.format(file['name']))
-                message = 'ファイルは削除しました。ファイルアップロードは禁止ですよ。'
-                self.post_message_to_channels(file['channels'], message)
             else:
                 print("{0} could not be deleted.".format(file['name']))
+
+        message = '月次点検のため、ファイルを削除しました。'
+        self.post_message_to_channels(channel_set, message)
 
     def post_message_to_channels(self, channels, message):
         """
@@ -50,10 +54,6 @@ class SlackPolice(object):
         for channel in channels:
             self.__slacker.chat.post_message(channel, message, username='', icon_url='')
 
-
-if __name__ == '__main__':
-    interval_sec = 10
+def slackpolice(event, context):
     police = SlackPolice()
-    while True:
-        police.delete_all_files()
-        time.sleep(interval_sec)
+    police.delete_all_files()
